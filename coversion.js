8d1ece99693ba2dda0075ec93943f00f31257969
Ditmar Castro <ditmarcastro@gmail.com>
2018-09-06 11:07:46 -0400

    decode terminado

--- coversion.js
+++ coversion.js
@@ -1,4 +1,6 @@
 var msn  = "Hola mundo"
+
+// a ti te toco lo mas facil
 console.log(codec(msn));
 function codec(msn) {
   var data = [];
@@ -6,4 +8,20 @@ function codec(msn) {
     data.push(msn.charCodeAt(i).toString(16));
   }
   return data.toString().replace(/,/g,"");
+}
+var msncode = "486f6c61206d756e646f";
+console.log(decode(msncode));
+function decode (msncode){
+	var data = []
+	for (var i = 0; i < msncode.length; i += 2) {
+		data.push(msncode.substr(i,2));
+	}
+	var cad = "";
+	for (var j = 0; j < data.length; j++) {
+		//console.log(data[j]);
+		var n = Number(parseInt(data[j], 16).toString(10));
+		cad += String.fromCharCode(n);
+		
+	} 
+	return cad;
 }
